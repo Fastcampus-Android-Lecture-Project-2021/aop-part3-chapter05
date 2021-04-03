c3f191d6422eb87bfc8c305e49a9d87dedeeb90c
kyeongwan <kang@kyeongwan.kr>
2021-04-03 16:12:24 +0900

    clip02. 이메일 로그인 구현하기

--- app/src/main/java/fastcampus/aop/part3/aop_part3_chapter05_final/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part3/aop_part3_chapter05_final/MainActivity.kt
@@ -1,11 +1,30 @@
 package fastcampus.aop.part3.aop_part3_chapter05_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.helloworldTextView).setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
